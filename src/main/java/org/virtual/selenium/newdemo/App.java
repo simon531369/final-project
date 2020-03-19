aea68e0e744b0ed1514b8e9068dbc580f6e4a459
simon531369 <simon531369@gmail.com>
2020-03-19 22:50:45 +0530

    f

---
+++ src/main/java/org/virtual/selenium/newdemo/App.java
@@ -0,0 +1,13 @@
+package org.virtual.selenium.newdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/virtual/selenium/newdemo/NewTest.java
@@ -0,0 +1,18 @@
+package org.virtual.selenium.newdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("hello");
+	  System.setProperty("webdriver.chrome.driver", "/home/rahul/Downloads/chrome/chromedriver");
+	  System.out.println("hello1");
+		WebDriver driver=new ChromeDriver();
+		 System.out.println("hello2");
+			driver.get("http://kushala.org/workspace/virtual-university/");
+	  
+  }
+}
